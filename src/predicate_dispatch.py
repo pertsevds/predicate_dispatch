e894e7243385baf807c71066c734063b6803a436
Dmitriy Pertsev <davaeron@gmail.com>
2022-11-06 23:34:16 +0300

    Added slots to class PredicatesList (#8)

--- src/predicate_dispatch.py
+++ src/predicate_dispatch.py
@@ -31,6 +31,9 @@ def _default_condition(*_: Any, **__: Any) -> bool:
 
 class PredicatesList:
     """Class for a list of predicate functions (lambda condition) and original code functions"""
+
+    __slots__ = ["_list", "_default_func_tuple"]
+
     def __init__(self) -> None:
         self._list: CallableList = []
         self._default_func_tuple: Optional[PredicateTuple] = None
