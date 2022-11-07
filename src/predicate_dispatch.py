bd0b1707bcff1972a180872b98de17993242b05c
Dmitriy Pertsev <davaeron@gmail.com>
2022-11-08 01:01:55 +0300

    Slot as tuple (#9)

--- src/predicate_dispatch.py
+++ src/predicate_dispatch.py
@@ -32,7 +32,7 @@ def _default_condition(*_: Any, **__: Any) -> bool:
 class PredicatesList:
     """Class for a list of predicate functions (lambda condition) and original code functions"""
 
-    __slots__ = ["_list", "_default_func_tuple"]
+    __slots__ = "_list", "_default_func_tuple"
 
     def __init__(self) -> None:
         self._list: CallableList = []
