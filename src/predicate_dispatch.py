d3a54c49d8d1e94daf2be5a0e2a5f88b6d2bedc9
Dmitriy Pertsev <davaeron@gmail.com>
2022-11-12 02:36:03 +0300

    fixed invalid syntax in 3.7

--- src/predicate_dispatch.py
+++ src/predicate_dispatch.py
@@ -15,9 +15,8 @@
 
 """Predicate dispatch"""
 
-from functools import wraps, lru_cache
-from typing import Any, Callable, Dict, List, Optional, Tuple, Iterator
-
+from functools import lru_cache, wraps
+from typing import Any, Callable, Dict, Iterator, List, Optional, Tuple
 
 ConditionFunction = Callable[[Any], bool]
 CodeFunction = Callable[[Any], Any]
@@ -32,7 +31,7 @@ def _default_condition(*_: Any, **__: Any) -> bool:
 class _PredicatesList:
     """Class for a list of predicate functions (lambda condition) and original code functions"""
 
-    __slots__: Tuple[str, str] = "_list", "_default_func_tuple"
+    __slots__: Tuple[str, str] = ("_list", "_default_func_tuple")
 
     def __init__(self) -> None:
         self._list: CallableList = []
