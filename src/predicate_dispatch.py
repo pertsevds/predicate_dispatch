c1b6f6f8d8c09f6e6b02a182cb4e312fe024b60c
Dmitriy Pertsev <davaeron@gmail.com>
2022-11-12 12:24:46 +0300

    Removed @wraps Found it not very useful and it eats away memory

--- src/predicate_dispatch.py
+++ src/predicate_dispatch.py
@@ -15,7 +15,7 @@
 
 """Predicate dispatch"""
 
-from functools import lru_cache, wraps
+from functools import lru_cache
 from typing import Any, Callable, Dict, Iterator, List, Optional, Tuple
 
 ConditionFunction = Callable[[Any], bool]
@@ -90,7 +90,6 @@ def predicate(condition: ConditionFunction = _default_condition) -> CodeFunction
     def wrapper(func: CodeFunction) -> CodeFunction:
         _add_callable(condition, func)
 
-        @wraps(func)
         def wrapped(*args: Any, **kwargs: Any) -> Any:
             resolved_callable: Optional[CodeFunction] = _resolve_callable(
                 func, *args, **kwargs
@@ -112,7 +111,6 @@ def predicate_cache(condition: ConditionFunction = _default_condition) -> CodeFu
     def wrapper(func: CodeFunction) -> CodeFunction:
         _add_callable(condition, func)
 
-        @wraps(func)
         def wrapped(*args: Any, **kwargs: Any) -> Any:
             resolved_callable: Optional[CodeFunction] = _resolve_callable_cached(
                 func, *args, **kwargs
@@ -137,7 +135,6 @@ def predicate_cache_result(
         _add_callable(condition, func)
 
         @lru_cache(maxsize=None, typed=True)
-        @wraps(func)
         def wrapped(*args: Any, **kwargs: Any) -> Any:
             resolved_callable: Optional[CodeFunction] = _resolve_callable(
                 func, *args, **kwargs
